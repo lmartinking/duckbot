9efacc7cadc3c8ab605654546505f8f6797c5bef
Lucas Martin-King <lmartinking@gmail.com>
2023-08-26 00:26:57 +1000

    Add fix for Raspberry Pi

--- run-bot.sh
+++ run-bot.sh
@@ -9,6 +9,12 @@ fi
 source .venv/bin/activate
 source .env
 
+# This appears to be necessary on Raspberry Pi, as certain Python libraries
+# use atomic instructions via libatomic (at least on ARM).
+if [[ -f /usr/lib/arm-linux-gnueabihf/libatomic.so.1 ]] ; then
+  export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1
+fi
+
 [[ ! -z "$CAPYCOIN_HOST" ]] && python -m duckbot.capycoin  # Setup tables
 
 python -m duckbot.bot
